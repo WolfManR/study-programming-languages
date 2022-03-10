3d3e2ae6ba663404fba6b0d05d531f35379ed9b6
Tarxos <ftarxos@gmail.com>
2022-03-10 22:55:27 +0300

    add vertical and horizontal lines figures

---
+++ CSharp/GeekBrains/SnakeInConsole/Figures/HorizontalLine.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakeInConsole.Figures;
+
+public class HorizontalLine : Figure
+{
+    public HorizontalLine(int xLeft, int xRight, int y, char sym)
+    {
+        pList = new List<Point>();
+        for (int x = xLeft; x <= xRight; x++)
+        {
+            Point p = new Point(x, y, sym);
+            pList.Add(p);
+        }
+    }
+}

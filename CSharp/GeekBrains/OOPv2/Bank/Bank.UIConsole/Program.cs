dd40c86da6939c0db8ef534d86c74d18fb962120
Tarxos <ftarxos@gmail.com>
2022-03-11 00:02:11 +0300

    add console project as sample for bank.core project logic

---
+++ CSharp/GeekBrains/OOPv2/Bank/Bank.UIConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using Bank.Core;
+
+using static System.Console;
+
+BankAccount bankAccount = new(5m);
+BankAccount bankAccount2 = new(BankAccountType.Deposit);
+BankAccount bankAccount3 = new(10.3m, BankAccountType.Credit);
+
+Print(bankAccount);
+Print(bankAccount2);
+Print(bankAccount3);
+
+bankAccount.Deposit(19.2m);
+bankAccount.Deposit(144.67m);
+bankAccount.TryWithdraw(120.2m);
+
+Print(bankAccount);
+
+static void Print(BankAccount bankAccount)
+{
+    WriteLine($"Bank account {bankAccount.Id} of type {bankAccount.Type}");
+    WriteLine($"Balance: {bankAccount.Balance}");
+}

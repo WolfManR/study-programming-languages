2aa4633d024344cc2200dfd16dd9f0897bb0c30f
Tarxos <ftarxos@gmail.com>
2022-03-11 00:27:16 +0300

    add Building model and it factory

---
+++ CSharp/GeekBrains/OOPv2/Buildings/Buildings.Core/Building.cs
@@ -0,0 +1,26 @@
+﻿namespace Buildings.Core;
+
+public sealed class Building
+{
+    private static int _idCounter;
+    private static int IdCounter => ++_idCounter;
+
+    private readonly int _id;
+    private double _height;
+    private int _numberOfStoreys;
+    private int _countOfEntrances;
+    private int _countOfApartments;
+
+    internal Building() => _id = IdCounter;
+
+    public int Id => _id;
+
+    public double Height { get => _height; set => _height = value; }
+    public int NumberOfStoreys { get => _numberOfStoreys; set => _numberOfStoreys = value; }
+    public int CountOfEntrances { get => _countOfEntrances; set => _countOfEntrances = value; }
+    public int CountOfApartments { get => _countOfApartments; set => _countOfApartments = value; }
+
+    public double CalculateHeightOfStorey() => Height / NumberOfStoreys;
+    public double CalculateNumberOfApartmentsInEntrace() => (double)CountOfApartments / CountOfEntrances;
+    public double CalculateNumberOfApartmentsOnStorey() => (double)CountOfApartments / NumberOfStoreys;
+}

4611c68a85044e8697243103f6c66499b0255cc1
Tarxos <ftarxos@gmail.com>
2022-03-10 23:59:11 +0300

    add bank.core project for main models

---
+++ CSharp/GeekBrains/OOPv2/Bank/Bank.Core/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿namespace Bank.Core
+{
+    public class BankAccount
+    {
+        private readonly Guid _id;
+        private readonly BankAccountType _type;
+
+        private decimal _balance;
+
+        public BankAccount(decimal initialBalance) : this(initialBalance, BankAccountType.Deposit) { }
+
+        public BankAccount(BankAccountType type) : this(0, type) { }
+
+        public BankAccount(decimal initialBalance, BankAccountType type)
+        {
+            if (initialBalance < 0) throw new ArgumentException("Initial balance cannot be lower that zero");
+            _balance = initialBalance;
+            _id = Guid.NewGuid();
+            _type = type;
+        }
+
+        public Guid Id => _id;
+        public BankAccountType Type => _type;
+        public decimal Balance => _balance;
+
+        public void Deposit(decimal sum)
+        {
+            _balance += sum;
+        }
+
+        public bool TryWithdraw(decimal sum)
+        {
+            if (_balance - sum < 0) return false;
+            _balance -= sum;
+            return true;
+        }
+    }
+}

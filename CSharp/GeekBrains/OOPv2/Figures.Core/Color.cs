8b83e0d8395ca0a823b3c39bee152af868b4610b
Tarxos <ftarxos@gmail.com>
2022-03-11 00:44:35 +0300

    add base figures

---
+++ CSharp/GeekBrains/OOPv2/Figures.Core/Color.cs
@@ -0,0 +1,21 @@
+﻿namespace Figures.Core;
+
+internal sealed class Color
+{
+    public byte Red { get; set; }
+    public byte Green { get; set; }
+    public byte Blue { get; set; }
+
+    public ConsoleColor ToConsoleColor()
+    {
+        return (Red, Green, Blue) switch
+               {
+                   (0, 0, 0)              => ConsoleColor.Black,
+                   (255, 255, 255)        => ConsoleColor.White,
+                   ( < 20, < 20, < 20)    => ConsoleColor.DarkGreen,
+                   ( < 70, < 70, < 70)    => ConsoleColor.Blue,
+                   ( < 140, < 140, < 140) => ConsoleColor.DarkRed,
+                   _                      => ConsoleColor.Magenta
+               };
+    }
+}
---
+++ CSharp/GeekBrains/OOPv2/Figures.Core/Figure.cs
@@ -0,0 +1,14 @@
+﻿namespace Figures.Core;
+
+internal abstract class Figure
+{
+    public Color Color { get; set; } = new Color();
+    public bool IsVisible { get; set; } = true;
+
+    public abstract void MoveHorizontal(int offset);
+    public abstract void MoveVertical(int offset);
+
+    public abstract void ChangeColor(Color color);
+
+    public abstract void Draw();
+}

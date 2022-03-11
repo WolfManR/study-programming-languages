1e2fa1f4a6fa3d7c02a84d3104141a1cff24f153
Tarxos <ftarxos@gmail.com>
2022-03-11 21:10:48 +0300

    add user settings manager project

---
+++ CSharp/GeekBrains/BeginnerCSharp/UserSettingsManager/UserInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+
+namespace UserSettingsManager;
+
+public sealed class UserInfo : SectionExtension
+{
+    [ConfigurationProperty(nameof(Name), DefaultValue = "", IsRequired = true)]
+    public string Name
+    {
+        get => (string)this[nameof(Name)];
+        set => SetWithThrowIfReadOnly(value);
+    }
+
+    [ConfigurationProperty(nameof(Age), DefaultValue = 0, IsRequired = true)]
+    [IntegerValidator(MinValue = 0, MaxValue = 140)]
+    public int Age
+    {
+        get => (int)this[nameof(Age)];
+        set => SetWithThrowIfReadOnly(value);
+    }
+
+
+    [ConfigurationProperty(nameof(Career), DefaultValue = "", IsRequired = false)]
+    public string Career
+    {
+        get => (string)this[nameof(Name)];
+        set => SetWithThrowIfReadOnly(value);
+    }
+
+    public override string ToString() => $"{Name} in {Age} age ,with career of {Career}";
+}

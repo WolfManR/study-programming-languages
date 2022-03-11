823317bd4fc274325ca5e1338f3d13af14dd979f
Tarxos <ftarxos@gmail.com>
2022-03-11 15:53:56 +0300

    add BeginnerCSharp course  HelloUser project

---
+++ CSharp/GeekBrains/BeginnerCSharp/HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+Write("Пожалуйста представьтесь: ");
+var userName = ReadLine();
+var name = (userName?[0].ToString().ToUpperInvariant() + userName?[1..].ToLowerInvariant()).Trim();
+
+WriteLine($"\r\nПривет, {(name is { Length: > 0 } user ? user : "Noname")}, сегодня {DateTime.Now:dd.MM.yyyy}");
+
+// Program Stop
+ReadLine();

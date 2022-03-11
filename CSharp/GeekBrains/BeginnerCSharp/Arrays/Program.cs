0704843b9d8d84f1433629539df84f4ee76c554d
Tarxos <ftarxos@gmail.com>
2022-03-11 17:04:42 +0300

    print diagonal array with numbers

---
+++ CSharp/GeekBrains/BeginnerCSharp/Arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+
+const byte size = 5;
+var array = new int[size, size];
+
+// Fill array
+for (var i = 0; i < size; i++)
+for (var j = 0; j < size; j++)
+    array[i, j] = i + j;
+
+// Print array
+for (var i = 0; i < size; i++)
+{
+    for (var j = 0; j < size; j++)
+        Write(array[i, j] + " ");
+    WriteLine();
+}
+
+WriteLine($"Input Pad from central diagonal, from [-{size - 1}..0..{size - 1}]");
+var input = ReadLine().AsSpan();
+
+if (!int.TryParse(input, out var number))
+{
+    WriteLine("incorrect input");
+    return;
+}
+
+if (number is <= -size or >= size)
+{
+    WriteLine("Incorrect number");
+    return;
+}
+
+int normalizedNumber = number < 0 ? Math.Abs(number) : number;
+var times = size - Math.Abs(number);
+//diagonal
+for (int i = 0, j = 0; i < times; i++, j++)
+{
+    var output = number switch
+                 {
+                     0   => array[i, j],
+                     < 0 => array[i, j + normalizedNumber],
+                     _   => array[i + normalizedNumber, j]
+                 };
+    Write(output + " ");
+}
+
+// Program stop
+WriteLine();

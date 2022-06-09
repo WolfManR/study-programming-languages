0e798c1ccaed72e99a9dca441d4e0f7336ab6089
Tarxos <ftarxos@gmail.com>
2022-06-09 03:21:18 +0300

    add test for algorithm that calculate longest common subsequence length

---
+++ CSharp/GeekBrains/Algorithms/SimpleAlgorithms/LongestCommonSubsequenceLengthTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace SimpleAlgorithms;
+
+public class LongestCommonSubsequenceLengthTests
+{
+    private static int LongestCommonSubsequenceLength(string a, string b)
+    {
+        if (a.Length == 0 || b.Length == 0)
+            return 0;
+        if (a[0] == b[0])
+            return 1 + LongestCommonSubsequenceLength(a.Substring(1), b.Substring(1));
+        return Math.Max(LongestCommonSubsequenceLength(a.Substring(1), b),
+            LongestCommonSubsequenceLength(a, b.Substring(1)));
+    }
+
+    [Theory]
+    [InlineData(7, "point to this", "pointless message")]
+    public void CalculateLongestSubsequenceLengthAsExpected(int expected, params string[] strings)
+    {
+        Assert.Equal(expected, LongestCommonSubsequenceLength(strings[0], strings[1]));
+    }
+}

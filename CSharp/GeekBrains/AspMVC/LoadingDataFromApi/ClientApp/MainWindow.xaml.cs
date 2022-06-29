59fe30fb6d70151f7167b123c5277b805db649a7
Tarxos <ftarxos@gmail.com>
2022-06-30 01:00:27 +0300

    add sample in which client wpf app loading data from api on request

---
+++ CSharp/GeekBrains/AspMVC/LoadingDataFromApi/ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Windows;
+using Core;
+
+namespace ClientApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private static HttpClient? _client;
+    private const string ApiUrl = Values.Url;
+
+    private void Load(object sender, RoutedEventArgs e)
+    {
+        var input = BoxCountOfEntriesToLoad.Text;
+        if (!int.TryParse(input, out var countOfEntriesToLoad) && countOfEntriesToLoad <= 0)
+        {
+            ErrorBlock.Text = "Count of Entries to load must be integer and greater that 0";
+            return;
+        }
+
+        _client ??= ConfigureClient();
+
+        Thread loader = new Thread(new ParameterizedThreadStart(LoadEntries));
+        loader.Start(new ThreadDTO() { Client = _client, CountOfEntriesToLoad = countOfEntriesToLoad });
+    }
+
+    private void LoadEntries(object? arg)
+    {
+        if (arg is not ThreadDTO threadDTO) return;
+        try
+        {
+            var response = threadDTO.Client.Send(new HttpRequestMessage(HttpMethod.Get, $"/entries/{threadDTO.CountOfEntriesToLoad}"));
+            if (!response.IsSuccessStatusCode) return;
+
+            var stream = response.Content.ReadAsStream();
+            using var readStream = new StreamReader(stream);
+
+            // TODO: there must be cut request on chunks !!!!!
+
+            var json = readStream.ReadToEnd();
+
+            var responses = JsonSerializer.Deserialize<IEnumerable<Entry>>(json, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+
+            Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                foreach (var dto in responses)
+                {
+                    DataView.Items.Add(dto);
+                }
+            }));
+        }
+        catch (Exception ex)
+        {
+            Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                ErrorBlock.Text = ex.Message;
+            }));
+        }
+    }
+
+    private HttpClient ConfigureClient()
+    {
+        var httpHandler = new SocketsHttpHandler { MaxConnectionsPerServer = 2 };
+        var client = new HttpClient(httpHandler, true)
+        {
+            BaseAddress = new Uri(ApiUrl)
+        };
+        return client;
+    }
+
+    private class ThreadDTO
+    {
+        public HttpClient Client { get; init; }
+        public int CountOfEntriesToLoad { get; init; }
+    }
+}
---
+++ CSharp/GeekBrains/AspMVC/LoadingDataFromApi/ServerApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Core;
+
+var host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(builder =>
+    {
+        builder.UseUrls(Values.Url);
+
+        builder.ConfigureServices(services => services.AddSingleton<EntryGenerator>());
+
+        builder.Configure(app =>
+        {
+            app.UseRouting();
+            app.UseEndpoints(e =>
+            {
+                var entryGenerator = e.ServiceProvider.GetRequiredService<EntryGenerator>();
+
+                e.MapGet("/entries/{count:int}", async context =>
+                {
+                    var input = context.Request.RouteValues["count"]?.ToString();
+                    if (input is null || !int.TryParse(input, out var count))
+                    {
+                        context.Response.StatusCode = 400;
+                        return;
+                    }
+
+                    await context.Response.WriteAsJsonAsync(entryGenerator.Generate(count));
+                });
+            });
+        });
+    })
+    .Build();
+
+await host.RunAsync();
+
+internal class EntryGenerator
+{
+    private static readonly Random Random = new Random();
+    private static int _lastIndex;
+    private static int GetIndex => _lastIndex++;
+    private static readonly string[] Words = new[] { "Product", "Automation", "Semaphore", "Sky", "Weather", "Program" };
+
+    public IEnumerable<Entry> Generate(int count = 1)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            yield return new Entry(GetIndex, Words[Random.Next(Words.Length)], Random.Next());
+        }
+    }
+}

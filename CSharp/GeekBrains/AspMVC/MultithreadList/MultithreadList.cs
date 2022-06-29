3f172ac5f773f0b27f23a38fa6ea7239abc6a617
Tarxos <ftarxos@gmail.com>
2022-06-30 00:02:42 +0300

    add project with sample of multithread list

---
+++ CSharp/GeekBrains/AspMVC/MultithreadList/MultithreadList.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Immutable;
+
+namespace MultithreadList;
+
+public class MultithreadList<T>
+{
+    private readonly List<T> _list = new List<T>();
+
+    private readonly object _editLock = new();
+    private readonly object _getLock = new();
+
+    public void Add(T item)
+    {
+        lock (_editLock)
+        {
+            lock (_getLock)
+            {
+                _list.Add(item);
+            }
+        }
+    }
+
+    public void Remove(T item)
+    {
+        lock (_editLock)
+        {
+            lock (_getLock)
+            {
+                if (_list.Count <= 0) return;
+                _list.Remove(item);
+            }
+        }
+    }
+
+    public IReadOnlyCollection<T> GetData()
+    {
+        lock (_getLock)
+        {
+            return _list.ToImmutableArray();
+        }
+    }
+
+    public T GetLast()
+    {
+        lock (_getLock)
+        {
+            return _list.Count > 0 ? _list[^1] : default;
+        }
+    }
+}

2f29fbfac6dd8a0421338ebd4fb80c8146de6b92
Tarxos <ftarxos@gmail.com>
2022-06-30 01:08:32 +0300

    add project that test Visitor pattern by painting shapes in console

---
+++ CSharp/GeekBrains/AspMVC/VisitorPainter/ConsoleDrawer.cs
@@ -0,0 +1,68 @@
+﻿using VisitorPainter.Shapes;
+
+namespace VisitorPainter;
+
+internal class ConsoleDrawer : Drawer
+{
+    private readonly char _symbol;
+
+    public ConsoleDrawer(char symbol)
+    {
+        _symbol = symbol;
+    }
+
+    public override void DrawRectangle(Rectangle rectangle)
+    {
+        string s = _symbol.ToString();
+        string space = "";
+        string temp = "";
+        for (int i = 0; i < rectangle.Width; i++)
+        {
+            space += " ";
+            s += _symbol;
+        }
+
+        for (int j = 0; j < rectangle.X; j++)
+            temp += " ";
+
+        s += _symbol + "\n";
+
+        for (int i = 0; i < rectangle.Height; i++)
+            s += temp + _symbol + space + _symbol + "\n";
+
+        s += temp + _symbol;
+        for (int i = 0; i < rectangle.Width; i++)
+            s += "═";
+
+        s += _symbol + "\n";
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.CursorLeft = rectangle.X;
+        Console.Write(s);
+        Console.ResetColor();
+    }
+
+    public override void DrawCircle(Circle circle)
+    {
+        var radius = circle.Radius;
+        var thickness = circle.Thickness;
+        double rIn = radius - thickness, rOut = radius + thickness;
+
+        for (double y = radius; y >= -radius; --y)
+        {
+            for (double x = -radius; x < rOut; x += 0.5)
+            {
+                double value = x * x + y * y;
+                if (value >= rIn * rIn && value <= rOut * rOut)
+                {
+                    Console.Write(_symbol);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CSharp/GeekBrains/AspMVC/VisitorPainter/Program.cs
@@ -0,0 +1,39 @@
+﻿using VisitorPainter;
+using VisitorPainter.Shapes;
+
+Console.WriteLine("Chose a shape");
+var shapes = new (string name, Func<Shape> ctor)[]
+{
+    ("Rectangle", () => new Rectangle()),
+    ("Circle", () => new Circle())
+};
+
+for (var i = 0; i < shapes.Length; i++)
+{
+    Console.WriteLine($"{i + 1}. {shapes[i].name}");
+}
+
+var shapeNumber = GetInteger(1, shapes.Length, "Choose number of shape");
+
+var shape = shapes[shapeNumber - 1].ctor();
+
+var drawer = new ConsoleDrawer('*');
+
+shape.Apply(drawer);
+
+Console.ReadLine();
+
+
+int GetInteger(int min, int max, string repeatMessage)
+{
+    while (true)
+    {
+        var input = Console.ReadLine();
+        if (int.TryParse(input, out var number) && number >= min && number <= max)
+        {
+            return number;
+        }
+
+        Console.WriteLine(repeatMessage);
+    }
+}
